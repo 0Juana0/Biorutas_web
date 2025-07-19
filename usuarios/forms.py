00c1787857b9ffc49312097b019fb208d1280a02
0Juana0 <juana.torres@unisarc.edu.co>
2025-07-19 16:28:39 -0500

    Modificación para la validación de contraseñas
    Se hizo la modificación para la validación de las contraseñas, funciona, pero no arroja la notificación de que es por esa razón.

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -9,10 +9,11 @@ class RegistroClienteForm(forms.ModelForm):
         model = RegistroCliente
         fields = ['nombre', 'apellido', 'numero_telefono', 'correo']
 
-        def clean(self):
-            cleaned_data = super().clean()
-            password = cleaned_data.get('contraseña')
-            confirm = cleaned_data.get('confirmar_contraseña')
-            if password and confirm and password != confirm: 
-                raise forms.ValidationError('Las contraseñas no coinciden.')
-            return cleaned_data
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('contraseña')
+        confirm = cleaned_data.get('confirmar_contraseña')
+        if password and confirm and password != confirm: 
+            self.add_error('confirmar_contraseña', 'Las contraseñas no coinciden.')
+        
+        return cleaned_data
